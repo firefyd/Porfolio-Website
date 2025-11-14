a9529811c19b9dafc8bd10446c0ab6e9d1e82bcd
firefyd <joshuakauri@gmail.com>
2025-11-14 21:40:33 +1300

    Added projects config

---
+++ site/src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const projectsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    tags: z.array(z.string()),
+    github: z.string().url().optional(),
+  }),
+});
+
+export const collections = {
+  projects: projectsCollection,
+};
