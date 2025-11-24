2c3ef5fcaa55eecd152c3407e869c60d6e67d95e
Joshua Phillips <joshuakauri@gmail.com>
2025-11-24 16:25:39 +1300

    Bug fix

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,20 +1,17 @@
 <script>
-    // ThemeToggle component import is removed since it's no longer used here.
 
-    // State to control the visibility of the mobile menu
     let isOpen = false;
 
-    // Data defined in the Astro component will be passed as a prop
+
     export let navLinks;
 
     function toggleMenu() {
         isOpen = !isOpen;
     }
 
-    // SVG for Hamburger icon (3 lines)
     const HamburgerIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>`;
 
-    // SVG for Close icon (X)
+
     const CloseIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
 </script>
 
