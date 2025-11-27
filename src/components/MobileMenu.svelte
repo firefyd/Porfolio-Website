9f0b1185430a8cab876728f4ffd722fa20dc9376
firefyd <joshuakauri@gmail.com>
2025-11-27 23:33:01 +1300

    Added smooth scroll

--- src/components/MobileMenu.svelte
+++ src/components/MobileMenu.svelte
@@ -26,23 +26,25 @@
 </script>
 
 <button 
-    on:click={toggleMenu} 
-    class="md:hidden p-2 rounded-lg text-text-main hover:text-accent transition focus-visible-ring z-[60]"
+    on:click|stopPropagation={toggleMenu} 
+    class="md:hidden p-2 rounded-lg text-text-main hover:text-accent transition focus-visible-ring z-[60] relative h-10 w-10 flex items-center justify-center"
     aria-label="Toggle navigation menu"
     aria-expanded={isOpen}
 >
-    {#if isOpen}
+
+    {#if !isOpen}
         <svg in:fade={{duration: 150}} out:fade={{duration: 150}} 
              xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 absolute" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
-    {:else}
+    {/if}
+
+    {#if isOpen}
         <svg in:fade={{duration: 150}} out:fade={{duration: 150}} 
              xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 absolute" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
     {/if}
-</button>
 
 {#if isOpen}
     <div 
